8ddc00eae5ae746a0ae7ad01dac2d2097f67f694
harry <gudtis9556@naver.com>
2024-08-10 14:49:10 +0900

    Chore: 주석, 로그 수정

--- app/src/main/java/abled/semina/dagger/MainActivity.kt
+++ app/src/main/java/abled/semina/dagger/MainActivity.kt
@@ -3,6 +3,7 @@ package abled.semina.dagger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.Component
 import dagger.Module
 import dagger.Provides
@@ -11,34 +12,45 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    //주입 대상 지정
     @Inject
     lateinit var numberRepository: NumberRepository
 
-//    @Inject
-//    lateinit var charRepository: CharRepository
+    @Inject
+    lateinit var charRepository: CharRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        (applicationContext as MyApplication).repository?.inject(this)
 
-        println(numberRepository.text)
+        // MainActivity 지정
+        //Module과 Component를 생성한뒤 프로젝트 Rebuild를 하면 Dagger+Component가 활성화 된다.
+        //component 초기화
+        DaggerRepositoryComponent.builder().build().inject(this)
 
+        //출력
+        Log.d("TAG", "mainActivity: ${numberRepository.text}")
+        Log.d("TAG", "mainActivity: ${charRepository.text}")
+
+
+        //Main2로 이동
         startActivity(Intent(this,MainActivity2::class.java))
 
     }
 }
 
 
+// Module을 연결하고, 주입할 위치를 지정하는 곳
 @Component(modules = arrayOf(RepositoryModule::class))
-interface Repository {
+interface RepositoryComponent {
     fun inject (app: MainActivity)
-//    fun inject (app: MainActivity2)
+    fun inject (app: MainActivity2)
 }
 
 
+// 주입할 의존성을 셋팅하는 곳
 @Module
-object RepositoryModule{
+class RepositoryModule{
 
     @Provides
     fun provideNumberRepository(): NumberRepository = NumberRepository()
@@ -48,10 +60,14 @@ object RepositoryModule{
 
 }
 
+//랜덤한 숫자를 text 변수에 갖고있는 클래스
 class NumberRepository {
         var text = run{ Random().nextInt(100).toString()}
 }
 
+//class NumberRepository @Inject constructor(){}
+
+//랜덤한 문자를 text 변수에 갖고 있는 클래스
 class CharRepository {
         var text = run{ Random().nextInt(100).toChar().toString()}
 }
--- app/src/main/java/abled/semina/dagger/MainActivity2.kt
+++ app/src/main/java/abled/semina/dagger/MainActivity2.kt
@@ -2,6 +2,7 @@ package abled.semina.dagger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import jakarta.inject.Inject
 
 class MainActivity2 : AppCompatActivity() {
@@ -15,9 +16,13 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        (applicationContext as MyApplication).repository?.inject(this)
 
-        println(numberRepository.text)
-        println(charRepository.text)
+        // MainActivity2 지정
+        //Module과 Component를 생성한뒤 프로젝트 Rebuild를 하면 Dagger+Component가 활성화 된다.
+        //component 초기화
+        DaggerRepositoryComponent.builder().build().inject(this)
+
+        Log.d("TAG", "mainActivity2: ${numberRepository.text}")
+        Log.d("TAG", "mainActivity2: ${charRepository.text}")
     }
 }
